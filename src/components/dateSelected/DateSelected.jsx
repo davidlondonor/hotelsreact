03b705904ad4e4649c3bc32fe9b1c5c13116b229
davidlucky@gmail.com <davidlucky@gmail.com>
2020-03-18 23:32:54 -0500

    basic structure

---
+++ src/components/dateSelected/DateSelected.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./dateSelected.css";
+
+const DateSelected = () => {
+    return (
+        <nav className="nav">
+            <div>
+                <p>Texto</p>
+            </div>
+        </nav>
+    );
+};
+
+export default DateSelected;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./header.css";
+
+const Header = () => {
+    return (
+        <header className="headerMain">
+            <div className="headerText">
+                <h1>Hoteles</h1>
+                <h4>
+                    desde el martes, 1 de enero de 2020 hasta el míercoles, 2 de
+                    enero de 2021
+                </h4>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
